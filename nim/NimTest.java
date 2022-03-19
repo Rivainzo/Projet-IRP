19cc53ef43c371c592b5ac91d4d1d54dc4011472
JulesLinard <79532003+JulesLinard@users.noreply.github.com>
2022-03-19 02:45:14 +0100

    test

--- nim/NimTest.java
+++ nim/NimTest.java
@@ -1,21 +1,23 @@
 package jeux.nim;
 
 import java.util.List;
+import jeux.nim.Nim;
+import jeux.MinimaxSearch;
 
 public class NimTest {
 
     public static void main(String[] args) {
         Nim game = new Nim(3, 10);
+        int state;
         MinimaxSearch<Integer, Integer> minimaxSearch = 
             MinimaxSearch.createFor(game);
 	
         state = game.getInitialState();
-	int action = -1;
-	System.out.println("Machine player, what is your action?");
+	    int action = -1;
+	    System.out.println("Machine player, what is your action?");
 	//
         action = minimaxSearch.makeDecision(state);
         System.out.println("Metrics for Minimax : " + minimaxSearch.getMetrics());
-	System.out.println("Chosen action is " + action);
-	
+	    System.out.println("Chosen action is " + action);
    }
 }
